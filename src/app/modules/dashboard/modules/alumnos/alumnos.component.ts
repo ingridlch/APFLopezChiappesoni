dd0570509209aedf637397ee8d8d0b21a3db9256
Ingrid Lopez <108765639+ingridlch@users.noreply.github.com>
2025-04-28 14:51:15 -0300

    Add modules y components para abm y lista de alumnos, navbar y toolbar

--- src/app/modules/dashboard/modules/alumnos/alumnos.component.ts
+++ src/app/modules/dashboard/modules/alumnos/alumnos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Alumno } from './models';
 
 @Component({
@@ -16,29 +16,36 @@ export class AlumnosComponent {
     {id: 1, nombre: 'Analía', apellido: 'Gervasoni', email: 'analia@gmail.com'},
     {id: 2, nombre: 'Benjamin', apellido: 'Hurtado', email: 'benja@gmail.com'},
     {id: 3, nombre: 'Cecilia', apellido: 'Alem', email: 'ceci@outlook.com'},
+    {id: 4, nombre: 'Demetrio', apellido: 'Zaragoza', email: 'demetrio@outlook.com'},
+    {id: 5, nombre: 'Ernestina', apellido: 'Curuzu', email: 'ernecu@yahoo.com.ar'},
+    /*{id: 6, nombre: 'Fernando', apellido: 'Furlan', email: 'ferfu@outlook.com'},
+    {id: 7, nombre: 'Gabriela', apellido: 'Lorenzon', email: 'gabi@gmail.com'},
+    {id: 8, nombre: 'Hilario', apellido: 'Cuestas', email: 'hilari@outlook.com'},*/
   ];
   
   constructor(private fb: FormBuilder){
     this.alumnoForm = this.fb.group({
       id: 0,
-      nombre: [''],
-      apellido: [''],
-      email: [''],
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      apellido: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
     })
   }
 
   onSubmit(){
+    if (this.alumnoForm.invalid) {
+      alert('Complete todos los valores del formulario correctamente');
+      return;
+    }  
     if(this.idActual===0){
       //nuevo
       const newAlumno = this.alumnoForm.value;
       newAlumno.id = (this.alumnos.length>0) ? this.alumnos[this.alumnos.length - 1].id + 1 : 1; 
       this.alumnos = [...this.alumnos, newAlumno];
     } else {
-      //editando
+      //edita
       this.alumnos = this.alumnos.map((al) => al.id === this.idActual ? { ...al, ...this.alumnoForm.value } : al);
     }
-    
-    console.log(this.alumnos);
 
     this.idActual = 0;
     this.alumnoForm.reset();
